278ab65fa7b8e6ad25a3346b3fdb0f66c8a9fa29
Vaibhav Gawad <gawadvaibhavv@gmail.com>
2023-11-23 01:55:31 +0530

    finaly over hush

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const mysql = require('mysql');
+// import fetch from 'node-fetch';
+
 
 app.use(express.json());
-
+const dbConnection =  mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'contact',
+  });
+  
+  dbConnection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL: ', err);
+    } else {
+      console.log('Connected to MySQL database successfully!');
+    //   console.log('getting contacts table');
+    //     dbConnection.query('SELECT * FROM contact', (error, results, fields) => {
+    //         if (error) {
+    //         console.error('Error getting contact from database: ', error);
+    //         } else {
+    //         console.log('Contacts: ', results);
+    //         }
+    //     });
+        
+    }
+  });
 // Create a contact in CRM or database
-app.post('/createContact', (req, res) => {
+app.post('/createContact', async (req, res) => {
     const { first_name, last_name, email, mobile_number, data_store } = req.body;
     console.log(req.body)
     console.log(first_name, last_name, email, mobile_number, data_store);
@@ -40,15 +65,28 @@ app.post('/createContact', (req, res) => {
             return res.status(500).json({ error: 'Failed to create contact in CRM' });
         });
     } else if (data_store === 'DATABASE') {
-        
-        return res.status(200).json({ message: 'Contact created in database' });
-    } else {
+        const sql = `INSERT INTO contact (first_name, last_name, email, mobile_number) VALUES (?, ?, ?, ?)`;
+        const values = [first_name, last_name, email, mobile_number];
+
+        return await dbConnection.query(sql, values, (error, results, fields) => {
+            if (error) {
+            console.error('Error creating contact in database: ', error);
+            } else {
+            console.log('Contact created in database: ', results);
+            return res.status(200).json({ message: 'Contact created in database' , data: results });
+            }
+        }
+        );
+    } 
+    
+    else {
+
         return res.status(400).json({ error: 'Invalid data_store value' });
     }
 });
 
 // Get a contact from CRM or database
-app.post('/getContact', (req, res) => {
+app.post('/getContact', async (req, res) => {
     const { contact_id, data_store } = req.body;
 
     if (!contact_id || !data_store) {
@@ -73,22 +111,28 @@ app.post('/getContact', (req, res) => {
             return res.status(500).json({ error: 'Failed to retrieve contact from CRM' });
         });
     } else if (data_store === 'DATABASE') {
-        const contact = {
-            id: contact_id,
-            first_name: 'John',
-            last_name: 'Doe',
-            email: 'john.doe@example.com',
-            mobile_number: '1234567890'
-        };
+        const sql = `SELECT * FROM contact WHERE contact_id = ?`;
+        const values = [contact_id];
 
-        return res.status(200).json({ contact });
+        
+
+       return await dbConnection.query(sql, values, (error, results, fields) => {
+            if (error) {
+            console.error('Error retrieving contact from database: ', error);
+            } else {
+            console.log('Contact retrieved from database: ', results);
+            return res.status(200).json({ contact: results });
+            }
+        }
+        );
+        
     } else {
         return res.status(400).json({ error: 'Invalid data_store value' });
     }
 });
 
 // Update a contact in CRM or database
-app.post('/updateContact', (req, res) => {
+app.post('/updateContact', async (req, res) => {
     const { contact_id, first_name, last_name, email, mobile_number, data_store } = req.body;
    
     if (!contact_id || !first_name || !last_name || !email || !mobile_number || !data_store) {
@@ -122,13 +166,27 @@ app.post('/updateContact', (req, res) => {
             console.error(error);
             return res.status(500).json({ error: 'Failed to update contact in CRM' });
         });
-    } else {
+    } else if(data_store === 'DATABASE'){
+        const sql = `UPDATE contact SET first_name = ?, last_name = ?, email = ?, mobile_number = ? WHERE contact_id = ?`;
+        const values = [first_name, last_name, email, mobile_number, contact_id];
+
+        return await dbConnection.query(sql, values, (error, results, fields) => {
+            if (error) {
+            console.error('Error updating contact in database: ', error);
+            } else {
+            console.log('Contact updated in database: ', results);
+            return res.status(200).json({ message: 'Contact updated in database' , updateData: results });
+            }
+        }
+        );
+    }else
+    {
         return res.status(400).json({ error: 'Invalid data_store value' });
     }
 });
 
 // Delete a contact from CRM or database
-app.post('/deleteContact', (req, res) => {
+app.post('/deleteContact', async (req, res) => {
     const { contact_id, data_store } = req.body;
    
     if (!contact_id || !data_store) {
@@ -145,7 +203,7 @@ app.post('/deleteContact', (req, res) => {
         })
         .then(response => {
             if (response.ok) {
-                return res.status(200).json({ message: 'Contact deleted from CRM' });
+                return res.status(200).json({ message: 'Contact deleted from CRM' , data: response });
             } else {
                 throw new Error('Failed to delete contact from CRM');
             }
@@ -155,8 +213,18 @@ app.post('/deleteContact', (req, res) => {
             return res.status(500).json({ error: 'Failed to delete contact from CRM' });
         });
     } else if (data_store === 'DATABASE') {
+        const sql = `DELETE FROM contact WHERE contact_id = ?`;
+        const values = [contact_id];
 
-        return res.status(200).json({ message: 'Contact deleted from DATABASE' });
+        return await dbConnection.query(sql, values, (error, results, fields) => {
+            if (error) {
+            console.error('Error deleting contact from database: ', error);
+            } else {
+            console.log('Contact deleted from database: ', results);
+            return res.status(200).json({ message: 'Contact deleted from database' , data: results });
+            }
+        }
+        );
     } else {
         return res.status(400).json({ error: 'Invalid data_store value' });
     }
